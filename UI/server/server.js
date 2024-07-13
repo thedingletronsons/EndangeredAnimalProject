0bf8e0384574d05918fae57f824d9d9907bac1a3
anirudh <getanirao@gmail.com>
2024-07-13 13:59:21 -0700

    Revert "UI fix"
    This reverts commit b118437c9d6eb55d10b743a23f0106f6e0fdf5e2.

--- UI/server/server.js
+++ UI/server/server.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const app = express()
 
-// if user types "localhost:5000/api" server the response with following json  
 app.get("/api", (req,res) => (
     res.json({"users": ["userOne", "userTwo", "userThree", "userFour"]})
 ))
 
-// start listening at port 5000
 app.listen(5000, () => (console.log("Server started on port 5000")))
