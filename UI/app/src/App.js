8b8bcbf58234c2bf265caaf55cfe58fb8102ed4b
anirudh <getanirao@gmail.com>
2024-07-15 18:54:33 -0700

    Commits

---
+++ UI/app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import './App.css';
+
+function App() {
+    return(
+        <div className="App">
+            <h1>Yo!</h1>
+        </div>
+    );
+}
+
+export default App;
