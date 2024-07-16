6e64d115c1f3af68ab086e402e8cf2bfa00dd5b0
anirudh <getanirao@gmail.com>
2024-07-16 08:51:58 -0700

    UI

--- UI/app/src/components/pages/DataBase.js
+++ UI/app/src/components/pages/DataBase.js
@@ -2,5 +2,5 @@ import React from 'react';
 import '../../App.css';
 
 export default function DataBase() {
-  return <h1 className='database'>DATABASE</h1>;
+  return <h1 className='database'>Database</h1>;
 }
